76a38ee2abad53f595024a4c2d07d1eb9dc2f1b9
Kervin <Kervin@Kervin-Laptop.home>
2012-07-16 16:06:00 -0700

    Versioned to 3.0 Fixed Mooshroom disappearance onShear

--- src/pgDev/bukkit/DisguiseCraft/listeners/DCCustomListener.java
+++ src/pgDev/bukkit/DisguiseCraft/listeners/DCCustomListener.java
@@ -1,11 +1,14 @@
 package pgDev.bukkit.DisguiseCraft.listeners;
 
+import org.bukkit.Material;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import pgDev.bukkit.DisguiseCraft.Disguise;
 import pgDev.bukkit.DisguiseCraft.DisguiseCraft;
+import pgDev.bukkit.DisguiseCraft.Disguise.MobType;
 import pgDev.bukkit.DisguiseCraft.injection.PlayerInvalidInteractEvent;
 
 public class DCCustomListener implements Listener {
@@ -22,6 +25,16 @@ public class DCCustomListener implements Listener {
 			if (attacked != null) {
 				// Do the attack
 				((CraftPlayer) event.getPlayer()).getHandle().attack(((CraftPlayer) attacked).getHandle());
+			}
+		} else {
+			if (event.getPlayer().getItemInHand().getType() == Material.SHEARS) {
+				Player clicked = plugin.getPlayerFromDisguiseID(event.getTarget());
+				if (clicked != null) {
+					Disguise disguise = plugin.disguiseDB.get(clicked.getName());
+					if (disguise.mob != null && disguise.mob == MobType.MushroomCow) {
+						((CraftPlayer) event.getPlayer()).getHandle().netServerHandler.sendPacket(disguise.getMobSpawnPacket(clicked.getLocation()));
+					}
+				}
 			}
 		}
 	}
