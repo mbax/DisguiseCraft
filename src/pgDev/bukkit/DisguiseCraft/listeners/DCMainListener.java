d4d82655192bc7c95617e6304f0f97a69b309b57
Devil Boy <Devil Boy@128.54.113.77>
2012-11-29 02:04:05 -0800

    Made world-change update execute next tick

--- src/pgDev/bukkit/DisguiseCraft/listeners/DCMainListener.java
+++ src/pgDev/bukkit/DisguiseCraft/listeners/DCMainListener.java
@@ -1,10 +1,7 @@
 package pgDev.bukkit.DisguiseCraft.listeners;
 
-import java.util.LinkedList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
-import net.minecraft.server.Packet;
 
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -16,7 +13,6 @@ import org.bukkit.event.player.*;
 import pgDev.bukkit.DisguiseCraft.DisguiseCraft;
 import pgDev.bukkit.DisguiseCraft.disguise.*;
 import pgDev.bukkit.DisguiseCraft.listeners.attack.InvalidInteractHandler;
-import pgDev.bukkit.DisguiseCraft.api.PlayerUndisguiseEvent;
 import pgDev.bukkit.DisguiseCraft.threading.NamedThreadFactory;
 import pgDev.bukkit.DisguiseCraft.update.DCUpdateNotifier;
 
@@ -111,43 +107,8 @@ public class DCMainListener implements Listener {
 	
 	@EventHandler
 	public void onPlayerWorldChange(PlayerChangedWorldEvent event) {
-		// World Change is like a join
-		plugin.showWorldDisguises(event.getPlayer());
-		
-		// Handle disguise wearer going through a portal
-		if (plugin.disguiseDB.containsKey(event.getPlayer().getName())) {
-			Player disguisee = event.getPlayer();
-			Disguise disguise = plugin.disguiseDB.get(disguisee.getName());
-			
-			// Packets
-			LinkedList<Packet> killPackets = new LinkedList<Packet>();
-			LinkedList<Packet> revivePackets = new LinkedList<Packet>();
-			killPackets.add(disguise.packetGenerator.getEntityDestroyPacket());
-			revivePackets.add(disguise.packetGenerator.getSpawnPacket(disguisee));
-			if (disguise.type.isPlayer()) {
-				killPackets.add(disguise.packetGenerator.getPlayerInfoPacket(disguisee, false));
-				revivePackets.add(disguise.packetGenerator.getPlayerInfoPacket(disguisee, true));
-			}
-    		
-			// Remove his disguise from the old world
-			plugin.undisguiseToWorld(event.getFrom(), disguisee, killPackets);
-			
-			if (disguise.hasPermission(disguisee)) {
-				// Show the disguise to the people in the new world
-				plugin.disguiseToWorld(disguisee.getWorld(), disguisee, revivePackets);
-			} else {
-				// Pass the event
-				PlayerUndisguiseEvent ev = new PlayerUndisguiseEvent(disguisee);
-				plugin.getServer().getPluginManager().callEvent(ev);
-				if (ev.isCancelled()) {
-					plugin.disguiseToWorld(disguisee.getWorld(), disguisee, revivePackets);
-				} else {
-					plugin.unDisguisePlayer(disguisee);
-					disguisee.sendMessage(ChatColor.RED + "You've been undisguised because you do not have permissions to wear that disguise in this world.");
-					return;
-				}
-			}
-		}
+		// Handle this next tick
+		plugin.getServer().getScheduler().runTask(plugin, new WorldChangeUpdater(plugin, event));
 	}
 	
 	@EventHandler
